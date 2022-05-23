ce63e7f42f1557b49b8dd47f99951817a429794d
RobIII <r.janssen@keensystems.eu>
2022-05-23 14:14:01 +0200

    Add PermissionException

---
+++ SFTPServer/Exceptions/PermissionException.cs
@@ -0,0 +1,12 @@
+﻿using SFTP.Enums;
+
+namespace SFTP.Exceptions;
+
+public class PermissionException : HandlerException
+{
+    public string? Reason { get; init; }
+
+    public PermissionException(string? reason = null)
+        : base(Status.PermissionDenied)
+        => Reason = reason;
+}
