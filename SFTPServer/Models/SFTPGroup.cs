c373db85fccfdbcb4f116e4e6ef7d886f03b6af4
RobIII <r.janssen@keensystems.eu>
2022-05-23 16:00:55 +0200

    Add Root/Nobody user/group (e.g. SFTPUser.Root, SFTPUser.Nobody, SFTPGroup.Root and SFTPGroup.Nobody).

--- SFTPServer/Models/SFTPGroup.cs
+++ SFTPServer/Models/SFTPGroup.cs
@@ -2,6 +2,9 @@
 
 public record SFTPGroup(uint Id, string Name) : SFTPIdentifier(Id, Name)
 {
+    public static readonly SFTPGroup Root = new(0);
+    public static readonly SFTPGroup Nobody = new(65534);
+
     public SFTPGroup(uint Id)
         : this(Id, LookupId(Id)) { }
 }
--- SFTPServer/Models/SFTPUser.cs
+++ SFTPServer/Models/SFTPUser.cs
@@ -2,6 +2,9 @@
 
 public record SFTPUser(uint Id, string Name) : SFTPIdentifier(Id, Name)
 {
+    public static readonly SFTPUser Root = new(0);
+    public static readonly SFTPUser Nobody = new(65534);
+
     public SFTPUser(uint Id)
         : this(Id, LookupId(Id)) { }
 }
